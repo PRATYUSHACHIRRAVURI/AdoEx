2415d6a25cb30f1cfdcca89d6ccc6ab2837b6743
Pratyusha Chirravuri <Pratyusha_Chirravuri@epam.com>
2020-02-25 11:57:44 +0530

    Add project files.

---
+++ AdoEx/Controllers/UserController.cs
@@ -0,0 +1,94 @@
+﻿using AdoEx.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdoEx.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Student/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        // GET: Student/Create
+        public ActionResult Create(Models.User s)
+        {
+
+         
+                // TODO: Add insert logic here
+                User s1 = new User();
+                s1.InsertRegDetails(s);
+
+                // return View();
+                return RedirectToAction("Prints");
+           
+        }
+        public ActionResult Prints()
+        {
+            User s2 = new User();
+            s2.ViewRegistrations();
+            return View();
+        }
+        // POST: Student/Create
+
+
+            // GET: Student/Edit/5
+            public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Student/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Student/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Student/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ AdoEx/Models/User.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+
+namespace AdoEx.Models
+{
+    public class User
+    {
+        public int ID{ set; get; }
+        public string Name{ set; get; }
+        public string Email{ set; get; }
+
+        SqlConnection con = new SqlConnection("Data Source=EPINHYDW014D;Integrated Security=true;Initial Catalog=Student");
+        SqlCommand cmd = new SqlCommand();
+
+        //Creating function to insert details
+        public string InsertRegDetails(User obj)
+        {
+            cmd.CommandText = "Insert into UserTable(Name,Email) values('" + obj.Name + "','" + obj.Email + "')";
+            cmd.Connection = con;
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+            return "Success";
+
+
+        }
+        public List<User> ViewRegistrations()
+        {
+            cmd.CommandText = "Select * from UserTable";
+            cmd.Connection = con;
+            con.Open();
+            SqlDataReader sdr = cmd.ExecuteReader();
+            List<User> ht = new List<User>();
+
+            while (sdr.Read())
+            {
+                ht.Add(new User { ID = Convert.ToInt32(sdr["ID"]), Name = sdr["Name"].ToString(), Email = sdr["Email"].ToString() });
+            }
+            return ht;
+        }
+        public void ViewRegistrationsUsingDataSet()
+        {
+            con.Open();
+            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM UserTable", con);
+            DataSet ds = new DataSet();
+            da.Fill(ds, "User1");
+            con.Close();
+            foreach (DataRow row in ds.Tables[0].Rows)
+            { Debug.WriteLine(string.Format("Name: {0} , Email: {1}", row["Name"], row["Email"])); }
+
+        }
+    }
+}
+       
+    
